d413443bbdb8b57df6555cfef0329ecf8a1f01bd
christ <christmokonda@gmail.com>
2021-07-22 14:58:26 +0200

    [ADD] Checbox dans les boutons collapse du menu

--- hackathon/resources/views/index.blade.php
+++ hackathon/resources/views/index.blade.php
@@ -7,12 +7,13 @@
                 {{-- Image --}}
                 <div class="col-6">
                     <div class="card-img">
-                        <img src="./public/images/Silicone%20Fin/Augarde-_0013_RUBBER-gris.png" width="100%" height="50%">
+                        <img src= "{{asset('images/SiliconeFin/rubbergris')}}">
                     </div>
                 </div>
                 {{-- Liste de boutons collapsibles --}}
                 <div class="col-6">
                     <div id="accordion">
+                        {{-- Collection --}}
                         <div class="card">
                             <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
@@ -21,13 +22,15 @@
                                     </button>
                                 </h5>
                             </div>
-
                             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
-                                </div>
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Steel Cool</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Golden Chic</label>
                             </div>
                         </div>
+                        {{-- Cadran --}}
                         <div class="card">
                             <div class="card-header" id="headingTwo">
                                 <h5 class="mb-0">
@@ -38,10 +41,32 @@
                             </div>
                             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Noir</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Blanc</label>
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option3">Orange Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option4">Rose Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option5">Bleu ciel Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option6" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option6">Vert Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option7" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option7">Rouge foncé Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option8" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option8">Kaki</label>
+                                    <input type="radio" class="btn-check" name="options" id="option9" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option9">Violet Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option10" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option10">Bleu foncé Sunray</label>
+                                    <input type="radio" class="btn-check" name="options" id="option11" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option11">Taupe</label>
                                 </div>
                             </div>
                         </div>
+                        {{-- Matière --}}
                         <div class="card">
                             <div class="card-header" id="headingThree">
                                 <h5 class="mb-0">
@@ -52,10 +77,14 @@
                             </div>
                             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Bracelet Cuir</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Bracelet Silicone</label>
                                 </div>
                             </div>
                         </div>
+                        {{-- Longueur --}}
                         <div class="card">
                             <div class="card-header" id="headingFour">
                                 <h5 class="mb-0">
@@ -66,10 +95,14 @@
                             </div>
                             <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Bracelet S-M</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Golden L-XL</label>
                                 </div>
                             </div>
                         </div>
+                        {{-- Style --}}
                         <div class="card">
                             <div class="card-header" id="headingFive">
                                 <h5 class="mb-0">
@@ -80,10 +113,14 @@
                             </div>
                             <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Bracelet Large</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Bracelet Fin</label>
                                 </div>
                             </div>
                         </div>
+                        {{-- Couleur--}}
                         <div class="card">
                             <div class="card-header" id="headingSix">
                                 <h5 class="mb-0">
@@ -94,14 +131,39 @@
                             </div>
                             <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
                                 <div class="card-body">
-                                    ok
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                                    <label class="btn btn-secondary" for="option1">Rouge</label>
+                                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option2">Marron</label>
+                                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option3">Orange</label>
+                                    <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option4">Bleu foncé</label>
+                                    <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option5">Noir</label>
+                                    <input type="radio" class="btn-check" name="options" id="option6" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option6">Taupe</label>
+                                    <input type="radio" class="btn-check" name="options" id="option7" autocomplete="off">
+                                    <label class="btn btn-secondary" for="option7">Blanc</label>
                                 </div>
+                            </div>
+                        </div>
+                        {{-- Gravure --}}
+                        <div class="card">
+                            <div class="card-header" id="headingSeven">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                        Gravure
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+                                <div class="card-body"></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
