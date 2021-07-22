a301f6b75f62e99be856c7a4bfb32306a293f1d9
LAMTAOUI <mlamtaoui1@myges.fr>
2021-07-22 15:07:42 +0200

    [api] config d'affichage

---
+++ hackathon/app/Http/Controllers/ItemConfigurationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Services\PayUService\Exception;
+
+class ItemConfigurationController extends Controller
+{
+    public function menu()
+    {
+        try {
+            $path = storage_path() . "/json/db.json";
+            $json = json_decode(file_get_contents($path), true);
+            return response()->json($json["menu"], 200);
+        } catch (\Exception $e) {
+            return response()->json(storage_path(), 400);
+        }
+    }
+    public function items()
+    {
+        try {
+            $path = storage_path() . "/json/db.json";
+            $json = json_decode(file_get_contents($path), true);
+            return response()->json($json["items"], 200);
+        } catch (\Exception $e) {
+
+            return response()->json(storage_path(), 400);
+        }
+    }
+}
